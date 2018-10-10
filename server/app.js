248916fcda66d5a5043a15bdabfa0d4d4789798d
System Administrator <root@Narendras-MacBook-Pro.local>
2018-10-11 00:51:15 +0530

    Completed Vue App Front + Back

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 
@@ -13,7 +14,14 @@ const posts = require('./routes/api/posts');
 
 app.use('/api/posts', posts);
 
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 const port = process.env.PORT || 5000;
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
 
 app.listen(port, () => {
     console.log("Server Started on PORT " + port);
